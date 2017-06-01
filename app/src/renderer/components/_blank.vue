90af277279cc0ae01341a822a709ada952ff9e93
Andrew Mitchell <brew@zentri.com>
2017-06-01 15:25:30 +1000

    blank component

---
+++ app/src/renderer/components/_blank.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class='burst'>
+  </div>
+</template>
+
+<script>
+  import store from 'renderer/vuex/store';
+  // import {bus} from '../app/app.js';
+
+
+  export default {
+    data() {
+      return {};
+    },
+    components: {},
+    methods: {},
+    created() {},
+    mounted() {},
+    updated() {},
+    destroyed() {}
+  };
+</script>
+
+<style lang="less">
+
+</style>
